479e904694f95afdb5ad349e295efea933b1386f
Jeff Buchbinder <jeff@ourexchange.net>
2012-10-22 15:30:07 -0400

    Allow non-integer values in the statsd packet.

--- main.go
+++ main.go
@@ -210,7 +210,7 @@ func submit() {
 func handleMessage(conn *net.UDPConn, remaddr net.Addr, buf *bytes.Buffer) {
 	var packet Packet
 	var sanitizeRegexp = regexp.MustCompile("[^a-zA-Z0-9\\-_\\.:\\|@]")
-	var packetRegexp = regexp.MustCompile("([a-zA-Z0-9_]+):([0-9]+)\\|(c|ms)(\\|@([0-9\\.]+))?")
+	var packetRegexp = regexp.MustCompile("([a-zA-Z0-9_]+):([0-9\\.]+)\\|(c|ms)(\\|@([0-9\\.]+))?")
 	s := sanitizeRegexp.ReplaceAllString(buf.String(), "")
 	for _, item := range packetRegexp.FindAllStringSubmatch(s, -1) {
 		value, err := strconv.Atoi(item[2])
