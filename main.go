3150ad883fbecb320d5d5a08b093abf6760a96f3
Jeff Buchbinder <jeff@ourexchange.net>
2012-10-23 11:31:54 -0400

    Fix from Vlad, move timers to float64

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ var (
 var (
 	In       = make(chan Packet, 10000)
 	counters = make(map[string]int)
-	timers   = make(map[string][]int)
+	timers   = make(map[string][]float64)
 	gauges   = make(map[string]int)
 )
 
@@ -58,11 +58,12 @@ func monitor() {
 			if s.Modifier == "ms" {
 				_, ok := timers[s.Bucket]
 				if !ok {
-					var t []int
+					var t []float64
 					timers[s.Bucket] = t
 				}
-				intValue, _ := strconv.Atoi(s.Value)
-				timers[s.Bucket] = append(timers[s.Bucket], intValue)
+				//intValue, _ := strconv.Atoi(s.Value)
+				floatValue, _ := strconv.ParseFloat(s.Value, 64)
+				timers[s.Bucket] = append(timers[s.Bucket], floatValue)
 			} else if s.Modifier == "g" {
 				_, ok := gauges[s.Bucket]
 				if !ok {
@@ -118,6 +119,21 @@ func submit() {
 			go gm.SendMetric(name, m_value, m_type, m_units, m_slope, m_ival, m_ival, m_grp)
 		}
 	}
+	gmSubmitFloat := func(name string, value float64) {
+		if useGanglia {
+			if *debug {
+				fmt.Printf("Ganglia send metric %s value %f\n", name, value)
+			}
+			m_value := fmt.Sprint(value)
+			m_units := "count"
+			m_type := uint32(gmetric.VALUE_DOUBLE)
+			m_slope := uint32(gmetric.SLOPE_BOTH)
+			m_grp := "statsd"
+			m_ival := uint32(*flushInterval * int64(2))
+
+			go gm.SendMetric(name, m_value, m_type, m_units, m_slope, m_ival, m_ival, m_grp)
+		}
+	}
 	if *gangliaAddress != "" {
 		gm = gmetric.Gmetric{
 			Host:  *gangliaSpoofHost,
@@ -149,9 +165,9 @@ func submit() {
 	now := time.Now()
 	buffer := bytes.NewBufferString("")
 	for s, c := range counters {
-		value := int64(c) / ((*flushInterval * int64(time.Second)) / 1e3)
+		value := float64(c) / float64((float64(*flushInterval)*float64(time.Second))/float64(1e3))
 		fmt.Fprintf(buffer, "stats.%s %d %d\n", s, value, now)
-		gmSubmit(fmt.Sprintf("stats_%s", s), uint32(value))
+		gmSubmitFloat(fmt.Sprintf("stats_%s", s), value)
 		fmt.Fprintf(buffer, "stats_counts.%s %d %d\n", s, c, now)
 		gmSubmit(fmt.Sprintf("stats_counts_%s", s), uint32(c))
 		counters[s] = 0
@@ -165,11 +181,11 @@ func submit() {
 	}
 	for u, t := range timers {
 		if len(t) > 0 {
-			sort.Ints(t)
-			min := t[0]
-			max := t[len(t)-1]
-			mean := min
-			maxAtThreshold := max
+			sort.Float64s(t)
+			min := float64(t[0])
+			max := float64(t[len(t)-1])
+			mean := float64(min)
+			maxAtThreshold := float64(max)
 			count := len(t)
 			if len(t) > 1 {
 				var thresholdIndex int
@@ -177,24 +193,24 @@ func submit() {
 				numInThreshold := count - thresholdIndex
 				values := t[0:numInThreshold]
 
-				sum := 0
+				sum := float64(0)
 				for i := 0; i < numInThreshold; i++ {
 					sum += values[i]
 				}
-				mean = sum / numInThreshold
+				mean = float64(sum) / float64(numInThreshold)
 			}
-			var z []int
+			var z []float64
 			timers[u] = z
 
-			fmt.Fprintf(buffer, "stats.timers.%s.mean %d %d\n", u, mean, now)
-			gmSubmit(fmt.Sprintf("stats_timers_%s_mean", u), uint32(mean))
-			fmt.Fprintf(buffer, "stats.timers.%s.upper %d %d\n", u, max, now)
-			gmSubmit(fmt.Sprintf("stats_timers_%s_upper", u), uint32(max))
-			fmt.Fprintf(buffer, "stats.timers.%s.upper_%d %d %d\n", u,
+			fmt.Fprintf(buffer, "stats.timers.%s.mean %f %d\n", u, mean, now)
+			gmSubmitFloat(fmt.Sprintf("stats_timers_%s_mean", u), mean)
+			fmt.Fprintf(buffer, "stats.timers.%s.upper %f %d\n", u, max, now)
+			gmSubmitFloat(fmt.Sprintf("stats_timers_%s_upper", u), max)
+			fmt.Fprintf(buffer, "stats.timers.%s.upper_%d %f %d\n", u,
 				*percentThreshold, maxAtThreshold, now)
-			gmSubmit(fmt.Sprintf("stats_timers_%s_upper_%d", u, *percentThreshold), uint32(maxAtThreshold))
-			fmt.Fprintf(buffer, "stats.timers.%s.lower %d %d\n", u, min, now)
-			gmSubmit(fmt.Sprintf("stats_timers_%s_lower", u), uint32(min))
+			gmSubmitFloat(fmt.Sprintf("stats_timers_%s_upper_%d", u, *percentThreshold), maxAtThreshold)
+			fmt.Fprintf(buffer, "stats.timers.%s.lower %f %d\n", u, min, now)
+			gmSubmitFloat(fmt.Sprintf("stats_timers_%s_lower", u), min)
 			fmt.Fprintf(buffer, "stats.timers.%s.count %d %d\n", u, count, now)
 			gmSubmit(fmt.Sprintf("stats_timers_%s_count", u), uint32(count))
 		}
@@ -214,13 +230,13 @@ func handleMessage(conn *net.UDPConn, remaddr net.Addr, buf *bytes.Buffer) {
 	var packet Packet
 	var value string
 	var sanitizeRegexp = regexp.MustCompile("[^a-zA-Z0-9\\-_\\.:\\|@]")
-	var packetRegexp = regexp.MustCompile("([a-zA-Z0-9_]+):([0-9\\.]+)\\|(c|ms)(\\|@([0-9\\.]+))?")
+	var packetRegexp = regexp.MustCompile("([a-zA-Z0-9_]+):(\\-?[0-9\\.]+)\\|(c|ms)(\\|@([0-9\\.]+))?")
 	s := sanitizeRegexp.ReplaceAllString(buf.String(), "")
 	for _, item := range packetRegexp.FindAllStringSubmatch(s, -1) {
 		value = item[2]
-		_, err := strconv.Atoi(item[2])
-		if err != nil {
-			if item[3] == "ms" {
+		if item[3] == "ms" {
+			_, err := strconv.ParseFloat(item[2], 32)
+			if err != nil {
 				value = "0"
 			} else {
 				value = "1"
